0ee0f7a8b54c3dca541c79c3d540311ce149067b
rameshvargupta <rameshvargupta230@gmail.com>
2026-03-28 01:26:25 +0530

    app logo added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,38 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "GT Shop",
+        short_name: "GTShop",
+        start_url: "/",
+        display: "standalone",
+        theme_color: "#0f172a",
+        background_color: "#ffffff",
+        icons: [
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
+  ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
