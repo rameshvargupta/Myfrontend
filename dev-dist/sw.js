4f84ad163eecb6620f8f5333fae66fc200f7ee99
rameshvargupta <rameshvargupta230@gmail.com>
2026-03-29 14:01:45 +0530

    frontedn updated

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.sj8d7l75mqo"
+    "revision": "0.mlpktq99n8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
